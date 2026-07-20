b2789063d3f9a530a40b90e46c59da1375c762ab
Matheus Beuren <matheusbeuren@gmail.com>
2026-07-20 13:43:53 -0300

    fix

--- card-12/my_structure/dags/forex_pipeline_aula.py
+++ card-12/my_structure/dags/forex_pipeline_aula.py
@@ -12,17 +12,19 @@ default_args = {
             "retries": 1,
             "retry_delay": timedelta(minutes=5)
         }
+# argumentos defauts utilizados no dag
 # define que na queremos nenhuma notificação sobre o airflow no email, e define tentaivas e tempo entre elas
 
 with DAG(dag_id="forex_data_pipeline_v_2", schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
-    
+    # parametros de definição dag
     is_forex_rates_available = HttpSensor(
         task_id="is_forex_rates_available",
         method="GET",
         http_conn_id="forex_api",
         endpoint="latest",
         response_check=lambda response: "rates" in response.text,
+        # retorna True se deu certo e False se deu erro
         poke_interval=5,
         timeout=20
     )
-    #teste para ver se a request vai dar cert
+    #teste de request
