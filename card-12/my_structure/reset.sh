f7411ecff1b1c5d9db9645a87816d1a6822080f4
Matheus Beuren <matheusbeuren@gmail.com>
2026-07-20 16:15:24 -0300

    reset

--- card-12/my_structure/reset.sh
+++
@@ -1,3 +0,0 @@
-# WARNING: apaga containers, volumes e dados do banco
-docker compose down -v
-rm -rf ./logs/*
--- card-12/my_structure/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker compose build && \
-docker compose up airflow-init && \
-docker compose up -d
